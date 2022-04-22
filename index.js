26527942fbfde84399af878543a42bf2b8a58428
Temidayo Olawoyin <t.olawoyin@outlook.com>
2022-04-22 08:31:31 +0100

    hello

--- index.js
+++ index.js
@@ -14,77 +14,16 @@ function Header() {
     )
 }
 
-function Form() {
-    return (
-        <form name="messageFromUser" netlify>
-            <div className="background" id= "form">
-                <div className="container">
-                    <div className="screen">
-                        <div className="screen-header">
-                            <div className="screen-header-left">
-                                <div clasNames="screen-header-button close"></div>
-                                <div className="screen-header-button maximize"></div>
-                                <div className="screen-header-button minimize"></div>
-                            </div>
-                            <div className="screen-header-right">
-                                <div className="screen-header-ellipsis"></div>
-                                <div className="screen-header-ellipsis"></div>
-                                <div className="screen-header-ellipsis"></div>
-                            </div>
-                        </div>
-                        <div className="screen-body">
-                            <div className="screen-body-item left">
-                                <div className="app-title">
-                                    <span>CONTACT</span>
-                                    <span>US</span>
-                                </div>
-                                <div className="app-contact">CONTACT INFO </div>
-                            </div>
-                            <div className="screen-body-item">
-                                <div className="app-form">
-                                    <div className="app-form-group">
-                                        <input type="text" class="app-form-control" placeholder="NAME" />
-                                    </div>
-                                    <div class="app-form-group">
-                                        <input className="app-form-control" type="email" placeholder="EMAIL" />
-                                    </div>
-                                <div className="app-form-group">
-                                    <input className="app-form-control" type="text" placeholder="CONTACT NO" />
-                                </div>
-                                <div className="app-form-group message">
-                                    <input className="app-form-control" type="text" placeholder="MESSAGE" />
-                                </div>
-                                <div className="app-form-group buttons">
-                                    
-                                    <button className="app-form-button" type="submit">Submit</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            {/* <div class="credits">
-        inspired by
-        <a class="credits-link" href="https://dribbble.com/shots/2666271-Contact" target="_blank">
-            <svg class="dribbble" viewBox="0 0 200 200">
-            <g stroke="#ffffff" fill="none">
-                <circle cx="100" cy="100" r="90" stroke-width="20"></circle>
-                <path d="M62.737004,13.7923523 C105.08055,51.0454853 135.018754,126.906957 141.768278,182.963345" stroke-width="20"></path>
-                <path d="M10.3787186,87.7261455 C41.7092324,90.9577894 125.850356,86.5317271 163.474536,38.7920951" stroke-width="20"></path>
-                <path d="M41.3611549,163.928627 C62.9207607,117.659048 137.020642,86.7137169 189.041451,107.858103" stroke-width="20"></path>
-            </g>
-            </svg>
-            Gururaj
-        </a>
-        </div>*/} 
-    </div> 
-    </div>
-</form>
-    )
-}
 
 function Main() {
     return(
         <main>
+            <section className="welcome--section">
+                <article><h1>Imagine a place...</h1>
+                <p>"... where you can locate your learning curve, locate friends around the globe with similar interest, Schooling Made Easy a community on discord is the way to go."</p></article>
+                
+                <button className="join-us-btn"><a  href="">Join us now</a></button>
+            </section>
             <section className="about--us" id="about-us">
                 <article>
                     <h1>About Us</h1>
@@ -99,7 +38,7 @@ function Main() {
                 <iframe src="https://discord.com/widget?id=966274062294843424&theme=dark" width="350" height="500"
                 allowtransparency="true" frameborder="0"
                 sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-                <button id="join-us-btn"><a href="https://discord.gg/JttcmZz8FM">Join Us Now</a></button>
+                <button className="join-us-btn"><a href="https://discord.gg/JttcmZz8FM">Join Us Now</a></button>
             </section>
         </main>
     )
@@ -110,7 +49,6 @@ function App() {
         <div id="main--div">
             <Header />
             <Main />
-            <Form />
         </div>
     )
 }
